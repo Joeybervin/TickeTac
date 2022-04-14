3b62afb51e97ef597b29b7fc887e7f0c1f3c2249
ClaireKarsenti <102292921+ClaireKarsenti@users.noreply.github.com>
2022-04-14 20:04:25 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -111,32 +111,31 @@ router.get('/logout', function(req, res, next) {
 
 // HOMEPAGE
 //
-router.post('/homepage',async  function(req, res, next) {
+router.get('/homepage',async  function(req, res, next) {
   if (req.session.user === null) {
     res.redirect('/')
   }else {
     var user = req.session.user
   }
 
-  // Recherche de trajet à une date précise
-  var departureExist = await journeyModel.find({ departure: req.body.departure });
-  var arrivalExist = await journeyModel.find({ arrival: req.body.destination });
-  var dateExist = await journeyModel.find({ departureTime: req.body.departureTime });
-    /* Si aucun train disponible pour la date sélectionnée => affichage page erreur */
-  if (dateExist == null) {
-    res.redirect('/searchError')
-    /* Si un ou plusieurs trajets possibles pour la date sélectionnée => affichage page trajets possibles */
-  }else if (dateExist && departureExist && arrivalExist){
-    res.redirect('/journeyspage')
-  };
-  
-  res.render('homepage', { title: 'TickeTac', user, dateExist, departureExist, arrivalExist });
+  res.render('homepage', { title: 'TickeTac', user });
 });
 
 // JOURNEYS PAGE
 //
-router.get('/journeyspage', function(req, res, next) {
-  res.render('journeyspage', { title: 'TickeTac'});
+router.post('/journeyspage', async function(req, res, next) {
+   // Recherche de trajet à une date précise
+   var departureExist = await journeyModel.find({ departure: req.body.departure });
+   var arrivalExist = await journeyModel.find({ arrival: req.body.destination });
+   var dateExist = await journeyModel.find({ departureTime: req.body.departureTime });
+     /* Si aucun train disponible pour la date sélectionnée => affichage page erreur */
+   if (dateExist == null) {
+     res.redirect('/searchError')
+     /* Si un ou plusieurs trajets possibles pour la date sélectionnée => affichage page trajets possibles */
+   }else if (dateExist && departureExist && arrivalExist){
+     res.redirect('/journeyspage')
+   };
+  res.render('journeyspage', { title: 'TickeTac', dateExist, departureExist, arrivalExist });
 });
 
 // MY LAST TRIP
