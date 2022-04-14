2f8670018d6569295933c518e8d574402ff782d7
Joey Bervin <61996287+Joeybervin@users.noreply.github.com>
2022-04-14 13:03:09 +0200

    homepage
    front-end de la homepage

--- routes/index.js
+++ routes/index.js
@@ -46,27 +46,25 @@ router.get('/save', async function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-// Cette route est juste une verification du Save.
-// Vous pouvez choisir de la garder ou la supprimer.
-router.get('/result', function(req, res, next) {
-
-  // Permet de savoir combien de trajets il y a par ville en base
-  for(i=0; i<city.length; i++){
-
-    journeyModel.find( 
-      { departure: city[i] } , //filtre
-  
-      function (err, journey) {
-
-          console.log(`Nombre de trajets au départ de ${journey[0].departure} : `, journey.length);
-      }
-    )
-
-  }
-
-
-  res.render('index', { title: 'Express' });
+router.get('/homepage', function(req, res, next) {
+  res.render('homepage', { title: 'TickeTac' });
 });
+
+router.get('/journeyspage', function(req, res, next) {
+  res.render('journeyspage', { title: 'TickeTac' });
+});
+
+router.get('/mylasttrip', function(req, res, next) {
+  res.render('mylasttrip', { title: 'TickeTac' });
+});
+
+router.get('/searchError', function(req, res, next) {
+  res.render('searchError', { title: 'TickeTac' });
+});
+
+router.get('/card', function(req, res, next) {
+  res.render('card', { title: 'TickeTac' });
+});
+
 
 module.exports = router;
