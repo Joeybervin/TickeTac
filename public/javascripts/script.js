5c5a660948c3531e375e281e69ef18652f484923
ClaireKarsenti <102292921+ClaireKarsenti@users.noreply.github.com>
2022-04-15 16:25:00 +0200

    Password

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,3 +9,25 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+/* Pour afficher le password au moment du sign-in
+function myFunction() {
+  var x = document.getElementById("myInputSI");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}*/
+
+
+/* Pour afficher le mot de passe Sign-in et Sign-up*/
+function myFunction(nameInput) {
+  var x = document.getElementById(nameInput);
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+        
